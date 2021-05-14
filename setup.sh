8d60b28961ed05e2af0051ec08c1a249a7942229
leichgardt <hellgate4548@yandex.ru>
2021-05-14 20:38:23 +0300

    setup script bugfix

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 echo "create virtual env"
-python3 -m venv venv
+python3.8 -m venv venv
 source venv/bin/activate
 echo "installing env"
 pip install -U pip setuptools
-pip install -r requirements.txt
 mkdir installations
 cd installations
 git clone https://github.com/carpedm20/emoji.git
 cd emoji
 echo "installing custom module 'emoji'"
 python setup.py install
+pip install -r requirements.txt
 cd ../../
+echo "cleaning tmp files"
 rm -rf installations
 echo ""
 echo "complete"
