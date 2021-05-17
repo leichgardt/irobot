d36bdc83871a64f978785628a60afcdbe9deac05
leichgardt <hellgate4548@yandex.ru>
2021-05-17 17:34:50 +0300

    transition to mongoDB

--- src/bot/bot_core.py
+++ src/bot/bot_core.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 
@@ -7,8 +7,9 @@ from src.utils import config
 
 API_TOKEN = config['tesseract']['token-iro-mega-bot']
 
+storage = MongoStorage(host='localhost', port=27017, db_name='aiogram_fsm')
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
