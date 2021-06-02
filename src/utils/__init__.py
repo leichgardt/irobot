687834e6bf48aa7d79c839323c690c8e6e64941d
leichgardt <hellgate4548@yandex.ru>
2021-06-02 21:17:45 +0300

    logger optimized

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .logger import logger, flogger, alogger
+from .logger import logger, alogger, aio_logger, init_logger
 from .configer import config
 from .requester import request
 from .toolkit import get_datetime, get_phone_number
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,12 +4,12 @@ from aiologger.handlers.streams import AsyncStreamHandler
 from aiologger.handlers.files import AsyncFileHandler
 from aiologger.formatters.base import Formatter
 
-f_format = '[%(asctime)s] %(levelname)s  \t%(filename)s(%(lineno)d) - %(funcName)s: %(message)s'
-s_format = '%(levelname)s: %(message)s'
+f_format = '[%(asctime)s] %(levelname)-8s %(filename)s(%(lineno)d) - %(funcName)s: %(message)s'
+s_format = '%(levelname)-9s %(message)s'
 logfile = '/var/log/iron/{}.log'
 
 
-def __init_logger(name, info_file_level=True):
+def init_logger(name, info_file_level=True):
     s_handler = logging.StreamHandler()
     s_handler.setLevel(logging.INFO)
     s_handler.setFormatter(logging.Formatter(s_format))
@@ -18,42 +18,24 @@ def __init_logger(name, info_file_level=True):
     f_handler.setFormatter(logging.Formatter(f_format))
 
     logger = logging.getLogger(name)
+    logger.handlers = []
     logger.addHandler(s_handler)
     logger.addHandler(f_handler)
     logger.setLevel(logging.INFO)
     return logger
 
 
-def __aio_logger(name):
+def aio_logger(name, loop=None):
     s_handler = AsyncStreamHandler(level=logging.INFO, formatter=Formatter(s_format))
     f_handler = AsyncFileHandler(logfile.format(name))
     f_handler.level = logging.INFO
     f_handler.formatter = Formatter(f_format)
-    logger = Logger.with_default_handlers(name=name)
-    logger.handlers = []
+    logger = Logger(name=name, loop=loop)
     logger.add_handler(s_handler)
     logger.add_handler(f_handler)
+    logger.level = logging.INFO
     return logger
 
 
-logger = __init_logger('irobot')
-flogger = __init_logger('irobot-web', False)
-alogger = __aio_logger('irobot')
-
-
-if __name__ == '__main__':
-    import asyncio
-
-    async def main():
-        logger.info('info')
-        logger.warning('warning')
-        logger.error('error')
-        logger.fatal('fatal')
-        await alogger.info('a info')
-        await alogger.warning('a warning')
-        await alogger.error('a error')
-        await alogger.fatal('a fatal')
-        await asyncio.sleep(1)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+logger = init_logger('irobot')
+alogger = aio_logger('irobot')
