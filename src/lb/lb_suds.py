3f941c71b9bd9a03d06672e96e7bf59cf4b0d51c
leichgardt <hellgate4548@yandex.ru>
2021-06-07 17:32:45 +0300

    async upgrade for lb_suds.py

--- src/lb/lb_suds.py
+++ src/lb/lb_suds.py
@@ -1,39 +1,44 @@
-from suds.client import Client
+import uvloop
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from suds.client import Client
 
-from src.utils import config, alogger, get_datetime, get_phone_number
+from src.utils import config, logger, alogger, get_datetime, get_phone_number
 
 
 class LBCore:
+    get_datetime = get_datetime
+    get_phone_number = get_phone_number
+
     def __init__(self):
         self.user = config['lanbilling']['user']
         self.__password = config['lanbilling']['password']
         self.agents = config['lanbilling']['agents']
         self._api_url = config['lanbilling']['url']
         self._api_location = config['lanbilling']['location']
-        self.client = None
-        self.get_datetime = get_datetime
-        self.get_phone_number = get_phone_number
-        self.loop = None
+        self.client: Client = None
+        self.executor = ThreadPoolExecutor(max_workers=10)
+        self.loop: uvloop.loop = None
 
     def connect_api(self):
         self.client = Client(url=self._api_url, location=self._api_location, faults=False)
 
-    async def login(self):
+    def login(self):
         start = datetime.now()
-        await alogger.info('API logging...')
+        logger.info('API logging...')
         if self.client is None:
             self.connect_api()
         if self.client.service.Login(self.user, self.__password)[0] == 200:
             end = datetime.now() - start
-            await alogger.info(f'API login success in {end}')
+            logger.info(f'API login success in {end}')
         else:
-            await alogger.warning('API logging error')
+            logger.warning('API logging error')
 
     def func(self, func, *args):
         return self.client.service['api3'][func](*args)
 
-    async def direct_request(self, func, *args, pass_faults=False):
+    async def async_direct_request(self, func, *args, pass_faults=False):
         if self.client is None:
             self.connect_api()
         try:
@@ -43,10 +48,9 @@ class LBCore:
         else:
             if res[0] != 200:
                 if res[1].faultstring == 'error_auth' or ('detail' in res[1] and 'No logged person' in res[1].detail):
-                    await self.login()
+                    self.login()
                 res = self.func(func, *args)
                 if res[0] != 200:
-                    await alogger.debug('\n'.join(['%s: %s' % (k, v) for k, v in dict(res[1]).items()]))
                     if pass_faults:
                         await alogger.info(res[1])
                         return []
@@ -54,9 +58,31 @@ class LBCore:
                         await alogger.error(res[1])
             return res[1]
 
+    def direct_request(self, func, *args, pass_faults=False):
+        if self.client is None:
+            self.connect_api()
+        try:
+            res = self.func(func, *args)
+        except Exception as e:
+            logger.error(f'[directRequest]\n{e}')
+        else:
+            if res[0] != 200:
+                if res[1].faultstring == 'error_auth' or ('detail' in res[1] and 'No logged person' in res[1].detail):
+                    self.login()
+                res = self.func(func, *args)
+                if res[0] != 200:
+                    if pass_faults:
+                        return []
+                    else:
+                        logger.error(res[1])
+            return res[1]
+
 
 lb = LBCore()
 
 
 async def lb_request(*args, **kwargs):
-    return await lb.direct_request(*args, **kwargs)
+    if lb.loop and lb.loop.is_running():
+        return await lb.loop.run_in_executor(lb.executor, partial(lb.direct_request, *args, **kwargs))
+    else:
+        return await lb.async_direct_request(*args, **kwargs)
