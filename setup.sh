e58ee6f67c38b74e8c8b997c697c5354025a1dea
leichgardt <hellgate4548@yandex.ru>
2021-06-21 19:52:24 +0300

    monstrous setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
-echo "create virtual env"
+echo "Checking requirement libs: python3.8 python3-pip python3-venv mongodb"
+for lib in python3.8 python3-pip python3-venv mongodb
+do
+if dpkg -s $lib >> /dev/null 2>&1
+then
+  continue
+else
+  echo "Error. Please, install lib $lib"
+  exit
+fi
+done
+echo "Creating virtual env"
 python3.8 -m venv venv
 . venv/bin/activate
 pip -V
-echo "installing env"
+echo "Installing packages"
 pip install -U pip setuptools wheel
 mkdir installations
-cd installations
+cd installations || return
 git clone https://github.com/carpedm20/emoji.git
-cd emoji
-echo "installing custom module 'emoji'"
+cd emoji || return
+echo "Installing custom module 'emoji'"
 python setup.py install
 cd ../../
 pip install -r requirements.txt
-echo "cleaning tmp files"
+echo "Cleaning tmp files"
 rm -rf installations
 echo ""
-echo "complete"
+echo "Complete"
