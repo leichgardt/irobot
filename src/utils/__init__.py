cb428f7957b50c2276cd2acdb30d258eae65c751
leichgardt <hellgate4548@yandex.ru>
2021-07-12 18:16:49 +0300

    requester __init__ import fix

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .logger import logger, alogger, aio_logger, init_logger
 from .configer import config
-from .requester import request
+from .requester import post_request
 from .toolkit import get_datetime, get_phone_number, map_format
