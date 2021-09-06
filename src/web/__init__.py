113a472f8d74581a470a79434bff44c6e28a3457
leichgardt <hellgate4548@yandex.ru>
2021-09-06 15:28:23 +0300

    import bugfix

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,6 +1,5 @@
 from .telegram_api import telegram_api, send_message, send_feedback
-from .api import (handle_payment_response, get_query_params, get_request_data, lan_require, handle_new_payment_request,
-                  broadcast, logining)
+from .api import get_query_params, get_request_data, lan_require, broadcast, logining
 from .monitors import auto_feedback_monitor, rates_feedback_monitor, auto_payment_monitor
 from .table import Table
 from .solo_worker import SoloWorker
