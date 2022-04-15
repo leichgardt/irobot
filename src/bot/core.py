42e0c2be55b2cb1627ccf53d0a8f8377c970ee89
leichgardt <hellgate4548@yandex.ru>
2022-04-15 17:01:33 +0300

    preparing for deployment

--- src/bot/core.py
+++ src/bot/core.py
@@ -1,6 +1,5 @@
 from aiogram import Bot
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 
 from parameters import BOT_TOKEN, MONGO_DB_HOST, MONGO_DB_PORT, MONGO_DB_NAME
@@ -10,4 +9,3 @@ storage = MongoStorage(host=MONGO_DB_HOST, port=MONGO_DB_PORT, db_name=MONGO_DB_
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=storage)
-dp.middleware.setup(LoggingMiddleware())
